15c1dadc9fced3dda96cb6d7e3b644c008e6141d
Cressence <cressencey@gmail.com>
2021-04-12 02:02:27 +0100

    Fix routing during deployment

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,17 +1,16 @@
-import {Fragment} from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
 import WeatherInfo from './pages/WeatherInfo';
 import Loading from './pages/Loading';
 
 const Routes = () => {
   const defaultPath = "/weather-app"
   return (
-    <Fragment>
-      <Switch>
-        <Route exact path={defaultPath} component={Loading} />
-        <Route exact path={`${defaultPath}/info`} component={WeatherInfo} />
-      </Switch>
-    </Fragment>
+      <Router>
+        <Switch>
+          <Route exact path={defaultPath} component={Loading} />
+          <Route exact path={`${defaultPath}/info`} component={WeatherInfo} />
+        </Switch>
+      </Router>
   );
 };
 
