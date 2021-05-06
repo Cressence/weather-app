2a6cf0272cf30f2e52c515b3093dac2614ed6214
Cressence <cressencey@gmail.com>
2021-05-06 13:59:00 +0100

    Complete usecase 2

--- src/pages/WeatherInfo/components/Weathercard/WeatherCard.tsx
+++ src/pages/WeatherInfo/components/Weathercard/WeatherCard.tsx
@@ -4,13 +4,16 @@ import './weatherCard.scss';
 
 interface WeatherProps {
     weatherInfo: Weather
+    active: boolean
 }
 
 const WeatherCard = (props: WeatherProps) => {
-    const { weatherInfo } = props;
+    const { weatherInfo, active } = props;
+    console.log(active);
+
     const selectedUnit = localStorage.getItem('unit');
     return (
-        <div className='weather-card-container'>
+        <div className={active ? 'weather-card-container active-card' : 'weather-card-container'}>
             <div className='weather-card-justify-content'>
                 <p className='temperature-text'>
                     <span> {selectedUnit === 'celcius' ? convertToCelcius(weatherInfo.temperature) : weatherInfo.temperature}</span> {' '} <span>
